0a82d74e8595cfe86ce7a714bb4651ce4505b455
Kerita <1332924387@qq.com>
2018-12-03 16:29:43 +0800

    feat: add missing file

--- src/transfer.js
+++ src/transfer.js
@@ -4,37 +4,37 @@ const whcomm = require('./common')
 
 // token 转账
 function transferToken(xprivkey, srcaddr, utxos, tokenid, tokennum, dstaddr) {
-  const hex_tokenid = sprintf.sprintf('%08s', tokenid.toString(16))
-  const hex_tokennum = sprintf.sprintf('%016s', tokennum.toString(16))
+  const hexTokenid = sprintf.sprintf('%08s', tokenid.toString(16))
+  const hexTokennum = sprintf.sprintf('%016s', tokennum.toString(16))
 
   const opreturn = `${whcomm.getWormHoleMagic()
     + whcomm.getWormHoleVersion()
   }0000${
-    hex_tokenid
-  }${hex_tokennum}`
+    hexTokenid
+  }${hexTokennum}`
 
   console.log(opreturn)
 
-  dstnum = coinfee.getMinOutputValue('BCH')
+  const dstnum = coinfee.getMinOutputValue('BCH')
   return whcomm.newWHTx(xprivkey, srcaddr, utxos, dstaddr, dstnum, opreturn)
 }
 
 
 // 转移所有token
 function transferAllToken(xprivkey, srcaddr, utxos, dstaddr, ecosystem) {
-  const hex_ecosystem = sprintf.sprintf('%02s', ecosystem.toString(16))
+  const hexEcosystem = sprintf.sprintf('%02s', ecosystem.toString(16))
 
   const opreturn = `${whcomm.getWormHoleMagic()
     + whcomm.getWormHoleVersion()
   }0004${
-    hex_ecosystem}`
+    hexEcosystem}`
 
   console.log(opreturn)
-  dstnum = coinfee.getMinOutputValue('BCH')
+  const dstnum = coinfee.getMinOutputValue('BCH')
   return whcomm.newWHTx(xprivkey, srcaddr, utxos, dstaddr, dstnum, opreturn)
 }
 
 module.exports = {
   transferToken,
-  transferAllToken
+  transferAllToken,
 }
